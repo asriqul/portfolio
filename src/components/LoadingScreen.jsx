dad2b05b752376803d0fc85f6101321a88e64dd2
asriqul <didipbnc11@gmail.com>
2025-09-09 02:58:07 +0700

    test update

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 
 export const LoadingScreen = ({onComplete}) => {
     const [text, setText] = useState("");
-    const fullText = "< Asriqul Hardi />";
+    const fullText = "< 0x-Asriqul Hardi />";
 
     useEffect(() => {
         let index = 0;
